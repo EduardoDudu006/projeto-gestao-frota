e31414f4dd8531a9544fa5b71240f7ce271eaa52
EDUARDO GONÇALVES DA LUZ SILVA <115898418+EduardoDudu006@users.noreply.github.com>
2026-05-31 15:37:42 -0300

    Hospedando projeto no Render

--- rotasync-ui/src/services/api.js
+++ rotasync-ui/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000/api", // URL do FastAPI local
+    baseURL: "https://projeto-gestao-frota.onrender.com/api", // URL do FastAPI local
 });
 
 export default api;
